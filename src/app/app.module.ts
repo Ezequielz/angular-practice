5f81b693ab07e56390a39c9e22265c4d23e8afdb
Ezequiel <zapata.ed1989@gmail.com>
2022-08-01 15:34:24 -0300

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Rutas principales
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
