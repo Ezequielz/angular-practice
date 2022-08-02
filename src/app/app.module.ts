b4cb239394b4738230b6a5be3b5b2bf4e05eaaa2
Ezequiel <zapata.ed1989@gmail.com>
2022-08-01 23:26:58 -0300

    main restored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsuariosComponent
+
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/index.ts
+++
@@ -1,7 +0,0 @@
-
-
-
-export { IResponse } from './api-response';
-export { Usuario } from './usuario'
-
-
